09c842decd2e38921d777de302d4a7c194cc7418
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-11-22 16:22:15 +0100

    [Refactoring][Gearbox]:Extract update "e" to separate method

--- Gearbox.java
+++ Gearbox.java
@@ -6,13 +6,17 @@ public class Gearbox {
     private int e = 0;
 
     public void doit(int i) {
-        e = i;
+        setE(i);
         if ((s == 0) || (i > 2000 && s < 6)) {
             s++;
         } else if (i < 500 && s > 1) {
             s--;
         }
 
+    }
+
+    private void setE(int e) {
+        this.e = e;
     }
 
 }
