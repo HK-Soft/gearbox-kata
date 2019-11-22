56f2d62e661912a62980e9133a9bc6bfef48a826
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-11-22 16:16:22 +0100

    [Refactoring][Gearbox]:Simplify the boolean logic on "i" & "s"

--- Gearbox.java
+++ Gearbox.java
@@ -11,15 +11,10 @@ public class Gearbox {
             s++;
             return;
         }
-        if (i > 2000) {
+        if (i > 2000 && s < 6) {
             s++;
-        } else if (i < 500) {
+        } else if (i < 500 && s > 1) {
             s--;
-        }
-        if (s > 6) {
-            s--;
-        } else if (s < 1) {
-            s++;
         }
 
     }
