1e9744ee25b68911a8abd4b369978d348e7507a0
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2019-11-22 16:06:23 +0100

    [Refactoring][Gearbox]:"e=i" always called, extract out of "if-else" block

--- Gearbox.java
+++ Gearbox.java
@@ -6,23 +6,22 @@ public class Gearbox {
     private int e = 0;
 
     public void doit(int i) {
+        e = i;
         if (s < 0) {
             // do nothing!
-            e = i;
-        }
-        else {
+        } else {
             if (s > 0) {
                 if (i > 2000) {
                     s++;
                 } else if (i < 500) {
                     s--;
                 }
-            } if (s > 6) {
+            }
+            if (s > 6) {
                 s--;
             } else if (s < 1) {
                 s++;
             }
-            e = i;
         }
     }
 
